ade3947e3951f9e60c7c3587e1b609aa71217ef1
Teresa Lasarow <teresa.lasarow@student.hpi.uni-potsdam.de>
2022-05-31 13:04:13 +0200

    run server with express

--- index.ts
+++ index.ts
@@ -1,31 +1,19 @@
-// const express = require('express');
+const express = require('express');
 const https = require('https');
 const http = require('http');
 const url = require('url');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv'); // is used to read environment variables
+
+dotenv.config();
 
 const host = 'localhost';
 const PORT = process.env.PORT || 3000;
-// const app = express();
+const app = express();
 
-const options = {
-    host: host,
-    path: '/evaluation',
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-};
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send('<h1>Hello from the TypeScript + Express world!</h1>');
+} );
 
-// Create a local server to receive data from
-const server = http.createServer();
-
-//Listen to the request event
-server.on('request', (request, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({
-        data: 'Hello from the TypeScript world!'
-    }));
-});
-
-server.listen(PORT, () => console.log(`Server is running on http://${host}:${PORT}`));
+app.listen(PORT, () => console.log(`[server]: Server is running at http://${host}:${PORT}`));
